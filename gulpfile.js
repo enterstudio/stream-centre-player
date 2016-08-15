f0a88b2fb48782f9c7dcf904ecbf42984762007e
EvAlex <evalexhimself@gmail.com>
2016-08-15 19:15:31 +0300

    add nuget:publish gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const watchify    = require("watchify");
 const gutil       = require("gulp-util");
 const runSequence = require('run-sequence');
 const clean       = require("gulp-clean");
+const nuget       = require("nuget");
+const es          = require("event-stream");
 
 const paths = {
     src: {
@@ -110,3 +112,20 @@ gulp.task('clean', () => {
     return gulp.src(paths.dest, {read: false})
         .pipe(clean());
 });
+
+gulp.task('nuget:publish', ['build'], () => {
+    return gulp.src("Qoollo.StreamCentre.Player.nuspec")
+        .pipe(es.map((file, cb) => {
+            nuget.pack(file, (err, nupkgFile) => {
+                if (err) {
+                    return cb(err);
+                }
+                nuget.push(nupkgFile, err => {
+                    if (err) {
+                        return cb(err);
+                    }
+                    cb();
+                })
+            });
+        }));
+})
